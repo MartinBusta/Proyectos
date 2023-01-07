e8233f92de0012330cb00de0be511531033a4100
unknown <sebastian.2006@gmail.com>
2023-01-07 15:57:01 -0300

    inicio

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+def print_hi(name):
+    df = pd.read_excel("datos33.xlsx")
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
